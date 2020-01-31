e3f76df65c431d7c6fa3fc96128d75a831714745
Austin Z. <137doctorwho2015@gmail.com>
2020-01-31 10:25:07 -0800

    test

---
+++ APCS-A-2020-AustinZhu/src/Unit0/Variables.java
@@ -0,0 +1,78 @@
+package Unit0;
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+public class Variables
+{
+	public static void main ( String[] args )
+	{
+		//define 1 variable of each of the
+		//following data types
+		//byte		short		int 		long
+		//float		double
+		//char      boolean		String
+
+		//integer variables
+		byte byteOne = 127;
+		int intOne = 2147483647;
+
+
+		//decimal variables
+		float floatOne = (float) 0.0000001;
+		double doubleOne = 0.000000000000001;
+
+
+		//other integer types
+		short shortOne = 32767;
+		long longOne = 2147483647;
+
+
+		//other types
+		char charOne = 'f';
+		boolean booleanOne = true;
+		String StringOne = "fdsaf";
+
+
+		//output your information here
+		System.out.println("/////////////////////////////////");
+		System.out.println("*                               *");
+		System.out.println("*        integer types          *");
+		System.out.println("*                               *");
+		System.out.println("*8 bit - byteOne = "+byteOne+"\t\t*");
+		System.out.println("*32 bit - intOne = "+intOne+"\t\t*");
+		System.out.println("*                               *");
+		System.out.println("*        decimal types          *");
+		System.out.println("*                               *");
+		System.out.println("*32 bit - intOne = "+intOne+"\t\t*");
+		System.out.println("*32 bit - intOne = "+intOne+"\t\t*");
+		System.out.println("*                               *");
+		System.out.println("*     other integer types       *");
+		System.out.println("*                               *");
+		System.out.println("*32 bit - intOne = "+intOne+"\t\t*");
+		System.out.println("*32 bit - intOne = "+intOne+"\t\t*");
+		System.out.println("*                               *");
+		System.out.println("*         other types           *");
+		System.out.println("*                               *");
+		System.out.println("*32 bit - intOne = "+intOne+"\t\t*");
+		System.out.println("*32 bit - intOne = "+intOne+"\t\t*");
+		System.out.println("*32 bit - intOne = "+intOne+"\t\t*");
+		System.out.println("/////////////////////////////////");
+
+
+
+
+
+
+
+
+
+
+
+
+	}
+}
