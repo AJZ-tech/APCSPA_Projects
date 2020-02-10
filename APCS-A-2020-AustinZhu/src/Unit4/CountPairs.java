d054e0bad9c62706c381588e86cb6e8a492b2a9f
Austin Z. <137doctorwho2015@gmail.com>
2020-02-10 10:09:22 -0800

    t

--- APCS-A-2020-AustinZhu/src/Unit4/CountPairs.java
+++ APCS-A-2020-AustinZhu/src/Unit4/CountPairs.java
@@ -10,6 +10,13 @@ public class CountPairs
 	public static int pairCounter( String str )
 	{ 
 		int count = 0;
+		
+		for (int i = 0; i < (str.length() - 1); i++) {
+			if (str.substring(i, i + 1).equals(str.substring(i + 1, i + 2))) {
+				count += 1;
+			}
+		}
+		
 		return count;
 	}
 }
