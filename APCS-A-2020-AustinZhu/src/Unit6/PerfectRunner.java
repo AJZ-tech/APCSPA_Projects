deb015731aef380b0b726de511684aead09da0c5
Austin Z. <137doctorwho2015@gmail.com>
2020-02-26 11:57:51 -0800

    t

--- APCS-A-2020-AustinZhu/src/Unit6/PerfectRunner.java
+++ APCS-A-2020-AustinZhu/src/Unit6/PerfectRunner.java
@@ -3,13 +3,54 @@ package Unit6;
 //www.apluscompsci.com
 //Name -
 
-import static java.lang.System.*; 
+import static java.lang.System.*;
+
+import java.util.Scanner; 
 
 public class PerfectRunner
 {
 	public static void main( String args[] )
 	{
-		//add test cases
-																
+		while (1==1) {
+			Scanner keyboard = new Scanner(in);
+			int num = 0;
+			while (1==1) {
+				out.print("Enter number :: ");
+				if (keyboard.hasNextInt()) {
+					num = keyboard.nextInt(); 
+					break;
+				} else {
+					out.println("\ninvalid input!");
+		            keyboard.next();
+				}
+			}
+			
+			//function
+			Perfect function = new Perfect(num);
+			if (function.isPerfect()) {
+				System.out.println(num + " is perfect");
+			} else {
+				System.out.println(num + " is not perfect");
+			}
+			
+			boolean quit = false;
+			while (1==1) {
+				out.print("Continue using? (y/n) :: ");
+				String answer = keyboard.next();
+				
+				if (answer.compareTo((String)"n") == 0) {
+					quit = true;
+					break;
+				} else if (answer.compareTo((String)"y") == 0) {
+					out.print("\n");
+					break;
+				} else {
+					out.print("invalid input!\n");
+				}
+			}
+			if (quit == true) {
+				break;
+			}
+		}														
 	}
 }
