1df1d85656eb07799e33ccebe24ee493bda4ec4d
Austin Z. <137doctorwho2015@gmail.com>
2020-02-27 11:32:10 -0800

    works

--- APCS-A-2020-AustinZhu/src/ElevensLab/Deck.java
+++ APCS-A-2020-AustinZhu/src/ElevensLab/Deck.java
@@ -13,33 +13,40 @@ public class Deck {
 
 	public static final int NUMCARDS = 52;
 	public static String[] SUITS = "CLUBS HEARTS DIAMONDS SPADES".split(" ");
+	//public static final int BUFFER = 1;
 	
 	private List<Card> cards;
 	private int top;
 
    //make a Deck constructor
 	public Deck () {
+	   	//refer cards to new ArrayList
 		cards = new ArrayList<Card>();
-		top = 51;
+	   	//set top to the top of the deck 51 (why 51?)
+		top = NUMCARDS;
 		
+	   	//loop through all suits
 		for (int i = 0; i < SUITS.length; i++) {
-			for (int a = 0; a < Card.getFaces().length; a++) {
+	   		//loop through all faces 1 to 13
+			for (int a = 1; a < Card.getFaces().length; a++) {
+	   			//add a new TwentyOneCard to the deck
 				cards.add(new Card(SUITS[i], a));
 			}
 		}
 	}
-   	//refer cards to new ArrayList
-   	//set top to the top of the deck 51
-   	
-   	//loop through all suits
-   		//loop through all faces 1 to 13
-   			//add a new TwentyOneCard to the deck
 
-   
-   //make a dealCard() method that returns the top card
+	   //make a dealCard() method that returns the top card
+   public Card dealCard() {
+	   top -= 1;
+	   return cards.get(top);
+   }
    
    //write a shuffle() method
-   	//use Colletions.shuffle
-   	//reset the top card 
+   public void shuffle() {
+	   	//use Colletions.shuffle
+	   Collections.shuffle(cards);
+	   	//reset the top card 
+	   top = NUMCARDS;
+   }
 	
 }
