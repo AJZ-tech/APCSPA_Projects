af73a65403f10c206b10ad5475fb9a164a0fbd59
mac <mac@192.168.0.103>
2020-04-10 18:01:38 -0700

    t

--- APCS-A-2020-AustinZhu/src/Unit13/WordSearch.java
+++ APCS-A-2020-AustinZhu/src/Unit13/WordSearch.java
@@ -12,11 +12,45 @@ public class WordSearch
 
     public WordSearch( int size, String str )
     {
+    	char[][] grid = new char[size][size];
+    	
+    	int counter = 0;
+    	for (int x = 0; x < size; x++) {
+    		for (int y = 0; y < size; y++) {
+    			grid[x][y] = str.charAt(counter);
+    			System.out.print(str.charAt(counter) + " ");
+    			counter += 1;
+    		}
+    		System.out.println();
+    	}
     }
 
     public boolean isFound( String word )
     {
-    	return false;
+    	boolean result = false;
+    	int WL = word.length();
+    	for (int x = 0; x < 8; x++) {
+    		for (int y = 0; y < 8; y++) {
+    			if (WL < x-1-WL) 
+    				result = checkRight(word, );
+    			if (word.length()) 
+    				result = checkLeft(word, );
+    			if (word.length()) 
+    				result = checkUp(word, );
+    			if (word.length()) 
+    				result = checkDown(word, );
+    			if (word.length() && word.length()) 
+    				result = checkDiagUpRight(word, );
+    			if (word.length() && word.length()) 
+    				result = checkDiagUpLeft(word, );
+    			if (word.length() && word.length()) 
+    				result = checkDiagDownLeft(word, );
+    			if (word.length() && word.length()) 
+    				result = checkDiagDownRight(word, );
+    		}
+    	}
+    	
+    	return result;
     }
 
 	public boolean checkRight(String w, int r, int c)
